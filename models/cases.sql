8e105ae75585240dc9e11dcc6b0c6868b61b51ef
giu-ferreira-cientista <giuferreira@gmail.com>
2022-05-15 22:06:18 +0000

    test sql

--- models/cases.sql
+++ models/cases.sql
@@ -1,3 +1,3 @@
 SELECT T.key, T.total_tested, T.total_confirmed, T.total_recovered , T.total_deceased from `stacklabs-payments.transations.transactions` as  T
 WHERE T.total_recovered > T.total_deceased 
-AND T.key = 'BR'
+AND T.key = 'CN_HA'

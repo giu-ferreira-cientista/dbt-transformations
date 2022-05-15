635227abf313c6cb494506cc9bcbc53d9e6a3b82
giu-ferreira-cientista <giuferreira@gmail.com>
2022-05-15 21:39:41 +0000

    adust sql

--- models/cases.sql
+++ models/cases.sql
@@ -1,4 +1,3 @@
-SELECT distinct(key), total_tested, total_confirmed, total_recovered , total_deceased from `stacklabs-payments.transations.transactions`
+SELECT key, total_tested, total_confirmed, total_recovered , total_deceased from `stacklabs-payments.transations.transactions`
 WHERE total_recovered > total_deceased 
-AND key = 'BR'
 AND total_tested is NOT NULL

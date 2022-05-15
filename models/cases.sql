0259e74ca5401405d0c6064fe646c5f5582a0ab7
giu-ferreira-cientista <giuferreira@gmail.com>
2022-05-15 21:43:47 +0000

    adjust sql

--- models/cases.sql
+++ models/cases.sql
@@ -1,3 +1,3 @@
 SELECT key, total_tested, total_confirmed, total_recovered , total_deceased from `stacklabs-payments.transations.transactions`
 WHERE total_recovered > total_deceased 
-AND total_tested is NOT NULL
+where key = 'BR'

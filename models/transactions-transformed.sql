b1af15c7ca8ea83b3718650c63ce475acf2c6712
giu-ferreira-cientista <giuferreira@gmail.com>
2022-07-17 20:26:23 +0000

    sql change

--- models/transactions-transformed.sql
+++ models/transactions-transformed.sql
@@ -1,4 +1,4 @@
-SELECT CAST(T.amt AS NUMERIC),T.dob,T.job,T.ssn,T.zip,T.city,T.last,T.first,T.state,T.cc_num,T.street,T.profile,T.acct_num,T.category,T.city_pop,T.is_fraud,T.merchant,T.merch_lat,T.trans_num,T.merch_long,T.trans_date,T.trans_time,
+SELECT CAST(T.amt AS NUMERIC) as amt,T.dob,T.job,T.ssn,T.zip,T.city,T.last,T.first,T.state,T.cc_num,T.street,T.profile,T.acct_num,T.category,T.city_pop,T.is_fraud,T.merchant,T.merch_lat,T.trans_num,T.merch_long,T.trans_date,T.trans_time,
 CASE
     WHEN gender = "M" THEN 1
     WHEN gender = "F" THEN 0
